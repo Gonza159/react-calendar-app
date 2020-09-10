473c3902a7534adc604af3771fbd8d3954dea9a3
Gonzalo S <gonzaa159@gmail.com>
2020-09-10 20:41:41 -0300

    agregue el build

---
+++ docs/precache-manifest.093ca7e70e1b08c0691f0074ddc8e2ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f63860a3f3c6ad4284b38c9fa570d836",
+    "url": "/index.html"
+  },
+  {
+    "revision": "baef830f70b6be697961",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "db0476ab78eded8bdc32",
+    "url": "/static/css/main.6020f20c.chunk.css"
+  },
+  {
+    "revision": "baef830f70b6be697961",
+    "url": "/static/js/2.65f13d4d.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.65f13d4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0476ab78eded8bdc32",
+    "url": "/static/js/main.2b58634d.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
